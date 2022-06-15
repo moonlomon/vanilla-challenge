58e9fbf83ffd0c2309ecf56275610b4968691573
SJMoon <103993019+CentiHei@users.noreply.github.com>
2022-06-15 14:43:38 +0900

    update

--- challenge/challenge 04/js/clock.js
+++ challenge/challenge 04/js/clock.js
@@ -4,10 +4,13 @@ function DdayClock() {
   const nowDay = new Date();
   const christmas = new Date("2022-12-25");
   const clock = christmas - nowDay;
-  const d = Math.floor(clock / (1000 * 60 * 60 * 24));
-  const h = Math.floor((clock / (1000 * 60 * 60)) % 24);
-  const m = Math.floor((clock / (1000 * 60)) % 60);
-  const s = Math.floor((clock / 1000) % 60);
+  const d = String(Math.floor(clock / (1000 * 60 * 60 * 24))).padStart(3, "0");
+  const h = String(Math.floor((clock / (1000 * 60 * 60)) % 24)).padStart(
+    2,
+    "0"
+  );
+  const m = String(Math.floor((clock / (1000 * 60)) % 60)).padStart(2, "0");
+  const s = String(Math.floor((clock / 1000) % 60)).padStart(2, "0");
 
   Dday.innerText = `${d}d ${h}h ${m}m ${s}s`;
 }
